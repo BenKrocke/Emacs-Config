e81853c11ddca3ce6290d0deeea14f34812a991e
Ben Krocke <benkrocke@gmail.com>
2023-09-19 17:21:04 +0930

    Add initial config

---
+++ init.el
@@ -0,0 +1,22 @@
+(use-package dracula-theme
+  :ensure t
+  :config
+  (load-theme 'dracula t))
+(use-package magit
+  :ensure t)
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(which-key magit dracula-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
